d5db3c6043bb25616bd9e418571fee76a589d32e
tsuki8 <3118181069@qq.com>
2026-03-09 14:40:08 +0800

    delete new example lean

--- GroebnerTac/Example.lean
+++ GroebnerTac/Example.lean
@@ -16,320 +16,215 @@ set_option linter.unusedTactic false in
 
 set_option synthInstance.maxSize 100000000
 
-open MvPolynomial
+
 variable {σ : Type*} (m : MonomialOrder σ)
 
-/- The test example of `remainder` -/
+/-The test example that verify mvpolynomial `f` is a remainder with respect to a set `G`-/
 example :
     lex.IsRemainder (X 0 ^ 2 + X 1 ^ 3 + X 2 ^ 4 + X 3 ^ 5: MvPolynomial (Fin 4) ℚ)
       {X 0, X 1, X 2, X 3} 0 := by
-      remainder[0 + C ↑(↑1 / ↑1) * X 0 ^ 1, 0 + C ↑(↑1 / ↑1) * X 1 ^ 2, 0 + C ↑(↑1 / ↑1) * X 2 ^ 3,
-        0 + C ↑(↑1 / ↑1) * X 3 ^ 4]
+  gb_solve
 
 example :
     lex.IsRemainder (X 0 ^ 2 - X 1 : MvPolynomial (Fin 3) ℚ)
       {X 0 ^ 2 - X 1} 0 := by
-    remainder
-
-example :
-    lex.IsRemainder (X 0 ^ 5 + X 1 * X 2 + 100 : MvPolynomial (Fin 3) ℚ)
-      {1} 0 := by
-    remainder
-
+  gb_solve
 
 example :
     lex.IsRemainder (X 0 ^ 2 * X 1 + X 0 * X 1 ^ 2 : MvPolynomial (Fin 3) ℚ)
       {X 0 * X 1 - 1} (X 0 + X 1) := by
-    remainder
-
-
-example :
-    lex.IsRemainder (X 0 : MvPolynomial (Fin 3) ℚ)
-      {2 * X 0 - 1} (C (1/2 : ℚ)) := by
-    remainder
-
+  gb_solve
 
 example :
     lex.IsRemainder (X 0 * X 1 : MvPolynomial (Fin 3) ℚ)
       {2 * X 0 - X 1}
       (C (1/2 : ℚ) * X 1 ^ 2) := by
-    remainder
-
-
-example :
-    lex.IsRemainder (X 0 ^ 2 + X 1 ^ 3 + X 2 ^ 4 + X 3 ^ 5: MvPolynomial (Fin 4) ℚ)
-      {X 0, X 1, X 2, X 3} 0 := by
-    remainder [X 0, X 1^2, X 2^3, X 3^4]
+  gb_solve
 
 example :
     lex.IsRemainder (X 0 ^ 2 + X 1 ^ 3 + X 2 ^ 4 + X 3 ^ 5: MvPolynomial (Fin 6) ℚ)
       {X 3, X 4 + X 5} (X 0 ^ 2 + X 1 ^ 3 + X 2 ^ 4) := by
-    remainder
-
-example :
-    lex.IsRemainder (X 0 + X 1 ^ 3 + X 2 ^ 4 + X 3 ^ 5: MvPolynomial (Fin 4) ℚ)
-      {X 0, X 1, X 2, X 3} 0 := by
-    remainder
-
-example :
-    lex.IsRemainder (X 0 + X 1 ^ 3 + X 2 ^ 4 : MvPolynomial (Fin 3) ℚ)
-      {X 0, X 1, X 2} 0 := by
-    remainder
-
-example :
-    lex.IsRemainder (X 0 ^ 2 + X 1 ^ 3 + X 2 ^ 4 + X 3 ^ 5: MvPolynomial (Fin 6) ℚ)
-      {X 3, X 4 + X 5} (X 0 ^ 2 + X 1 ^ 3 + X 2 ^ 4) := by
-    remainder
-
-example :
-    lex.IsRemainder (1: MvPolynomial (Fin 3) ℚ)
-      {X 0, X 1, 2 - X 3} (1) := by
-    remainder
-
+  gb_solve
 
 example :
     lex.IsRemainder (C (2/3 : ℚ) * X 0 * X 1 : MvPolynomial (Fin 3) ℚ)
       {3 * X 0 + 1}
       (- C (2/9 : ℚ) * X 1) := by
-    remainder
-
+  gb_solve
 
 example :
     lex.IsRemainder (X 0 ^ 2 + X 1 ^ 2 : MvPolynomial (Fin 3) ℚ)
       {X 0 + C (1/2 : ℚ) * X 1}
       (C (5/4 : ℚ) * X 1 ^ 2) := by
-    remainder
+  gb_solve
 
-
-/- The test example of basis -/
+/-The test example that verify a set is groebner basis-/
 example :
     letI basis := ({X 0 + X 1 ^ 2, X 1 ^ 2} : Set <| MvPolynomial (Fin 3) ℚ)
     lex.IsGroebnerBasis basis (Ideal.span basis) := by
-    basis
+    gb_solve
 
 example :
     letI basis := ({X 0 ^ 4 - X 1} : Set <| MvPolynomial (Fin 3) ℚ)
     lex.IsGroebnerBasis basis (Ideal.span basis) := by
-      basis
+    gb_solve
 
 example :
     letI basis := ({X 0} : Set <| MvPolynomial (Fin 3) ℚ)
     lex.IsGroebnerBasis basis (Ideal.span basis) := by
-    basis
-
-
-/- The test example of basis'-/
-set_option maxHeartbeats 20000000 in
-example :
-  lex.IsGroebnerBasis
-  ({X 1^3 - X 2^2, X 0^2 - X 1, X 0*X 1 - X 2, X 0*X 2 - X 1^2} :
-    Set <| MvPolynomial (Fin 3) ℚ)
-  (Ideal.span ({X 0^2 - X 1, X 0^3 - X 2} :
-    Set <| MvPolynomial (Fin 3) ℚ)):= by
-    basis'
-
+    gb_solve
 
 example :
   lex.IsGroebnerBasis
   ({X 0 - 1, X 1^2} : Set <| MvPolynomial (Fin 2) ℚ)
   (Ideal.span ({X 0^2 + X 1^2 - 1, X 0 - 1} : Set <| MvPolynomial (Fin 2) ℚ)) := by
-  basis'
+  gb_solve
 
 example :
   lex.IsGroebnerBasis
   ({1} : Set <| MvPolynomial (Fin 3) ℚ)
   (Ideal.span ({X 0, 1 - X 0} : Set <| MvPolynomial (Fin 3) ℚ)) := by
-  basis'
+  gb_solve
 
 example :
   lex.IsGroebnerBasis
   ({X 0 ^ 2 - 1} : Set <| MvPolynomial (Fin 2) ℚ)
   (Ideal.span ({X 0 ^ 2 - 1, (X 0 ^ 2 - 1) * (X 1 + 1)} :
     Set <| MvPolynomial (Fin 2) ℚ)) := by
-    basis'
-
-set_option maxHeartbeats 20000000 in
-example :
-  lex.IsGroebnerBasis
-  ({X 0 - X 3, X 1 - X 3, X 2 - X 3} : Set <| MvPolynomial (Fin 4) ℚ)
-  (Ideal.span ({X 0 - X 1, X 1 - X 2, X 2 - X 3} :
-    Set <| MvPolynomial (Fin 4) ℚ)) := by
-    basis'
+  gb_solve
 
 example :
   lex.IsGroebnerBasis
   ({X 0 - C (1/2 : ℚ)} : Set <| MvPolynomial (Fin 1) ℚ)
   (Ideal.span ({2 * X 0 - 1} : Set <| MvPolynomial (Fin 1) ℚ)) := by
-  basis'
+  gb_solve
 
 example :
   lex.IsGroebnerBasis
   ({X 1 + 6} : Set <| MvPolynomial (Fin 2) ℚ)
   (Ideal.span ({C (2/3 : ℚ) * X 1 + 4} : Set <| MvPolynomial (Fin 2) ℚ)) := by
-  basis'
+  gb_solve
+
+-- set_option maxHeartbeats 2000000 in
+-- example :
+--   lex.IsGroebnerBasis
+--   ({X 1^3 - X 2^2, X 0^2 - X 1, X 0*X 1 - X 2, X 0*X 2 - X 1^2} :
+--     Set <| MvPolynomial (Fin 3) ℚ)
+--   (Ideal.span ({X 0^2 - X 1, X 0^3 - X 2} :
+--     Set <| MvPolynomial (Fin 3) ℚ)):= by
+--     gb_solve
+
+/-The test example that verify ideal membership problem-/
+example :
+  X 0 ∈ Ideal.span ({X 0, X 1} : Set (MvPolynomial (Fin 2) ℚ)) := by
+  gb_solve
+
+example :
+   X 2 ∉ Ideal.span ({X 0 + X 1^ 2, X 1 ^ 2} : Set (MvPolynomial (Fin 3) ℚ)) := by
+  gb_solve
+
+example :
+   X 1 ∉ Ideal.span ({X 0 + X 1^ 2, X 1 ^ 2} : Set (MvPolynomial (Fin 3) ℚ)) := by
+  gb_solve
+
+example :
+   X 0 + X 1 ∉ Ideal.span ({X 0 + X 1^ 2, X 1 ^ 2} : Set (MvPolynomial (Fin 3) ℚ)) := by
+  gb_solve
+
+example :
+  X 0 + 2 * X 1^2 ∈ Ideal.span ({X 0 + X 1^ 2, X 1 ^ 2} : Set (MvPolynomial (Fin 2) ℚ)) := by
+  gb_solve
+
+example :
+  X 0 ∈ Ideal.span ({X 0 + X 1^ 2, X 1 ^ 2} : Set (MvPolynomial (Fin 2) ℚ)) := by
+  gb_solve
+
+example : 1 ∉ Ideal.span  ({X 1*X 3 + 1, X 0 + X 1} : Set <| MvPolynomial (Fin 4) ℚ) := by
+  gb_solve
+
+example : 1 ∉ Ideal.span  ({X 0 + X 1, 1 - X 0*X 3} : Set <| MvPolynomial (Fin 4) ℚ) := by
+  gb_solve
+
+example : 1 ∉ Ideal.span  ({X 1*X 3 + 1, X 0 + X 1} : Set <| MvPolynomial (Fin 4) ℚ) := by
+  gb_solve
+
+example : 1 ∈ Ideal.span ({X 0, X 1, 1 - X 3 * X 0} : Set <| MvPolynomial (Fin 4) ℚ) := by
+  gb_solve
+
+example :
+  X 0 ^ 3 - X 0 ∈ Ideal.span ({X 0 ^ 2 - 1} : Set (MvPolynomial (Fin 1) ℚ)) := by
+  gb_solve
 
 
+/-The test example that verify radical membership problem-/
+example :
+  X 0 * X 1 ∈ (Ideal.span ({C (1/2 : ℚ) * (X 0 + X 1), C (1/2 : ℚ) * (X 0 - X 1)} :
+   Set (MvPolynomial (Fin 2) ℚ))).radical := by
+  gb_solve
+
+example :
+  C (1/2 : ℚ) * X 0 + C (2/3 : ℚ) * X 1 ∈
+    (Ideal.span ({C (1/4 : ℚ) * X 0^2, C (4/9 : ℚ) * X 1^2} :
+    Set (MvPolynomial (Fin 2) ℚ))).radical := by
+  gb_solve
+
+example :
+  C (1/2 : ℚ) * X 0 + C (1/3 : ℚ) * X 1 ∈
+  (Ideal.span ({C (1/4 : ℚ) * X 0^2 + C (1/3 : ℚ) * X 0 * X 1 + C (1/9 : ℚ) * X 1^2}
+  : Set (MvPolynomial (Fin 2) ℚ))).radical := by
+  gb_solve
+
+example :
+  X 0 ∈ (Ideal.span ({X 0 + C (3/5 : ℚ) * X 1, C (3/5 : ℚ) * X 1}
+  : Set (MvPolynomial (Fin 2) ℚ))).radical := by
+  gb_solve
+
+example :
+  X 0 * X 1 ∈ (Ideal.span ({C (1/2 : ℚ) * (X 0 + X 1), C (1/2 : ℚ) * (X 0 - X 1)} :
+   Set (MvPolynomial (Fin 2) ℚ))).radical := by
+  gb_solve
+
+example :
+  X 2 ∉ (Ideal.span ({X 0, X 1} : Set (MvPolynomial (Fin 3) ℚ))).radical := by
+  gb_solve
+
+example :
+  X 0 ∉ (Ideal.span ({X 0 + X 1} : Set (MvPolynomial (Fin 3) ℚ))).radical := by
+  gb_solve
+
+example :
+  (X 0 - X 1) * (X 0 + X 1) ∈
+    (Ideal.span ({X 0^2, X 1^2} : Set (MvPolynomial (Fin 2) ℚ))).radical := by
+  gb_solve
+
+example :
+  C (1/2 : ℚ) * X 0 ∈
+    (Ideal.span ({C (1/4 : ℚ) * X 0} : Set (MvPolynomial (Fin 1) ℚ))).radical := by
+  gb_solve
+
+example :
+  X 0 ∈ (Ideal.span ({X 0 + C (2/3 : ℚ) * X 1, C (1/5 : ℚ) * X 1}
+    : Set (MvPolynomial (Fin 2) ℚ))).radical := by
+  gb_solve
+
+/- Some examples that compute the GB and provide a hypothesis in cases where the GB is not known -/
 example : lex.IsGroebnerBasis ({X 0, X 1} :
  Set (MvPolynomial (Fin 3) ℚ)) (Ideal.span {X 0, X 0 + X 1}) := by
   add_gb_hyp h ({X 0, X 0 + X 1} : Set (MvPolynomial (Fin 3) ℚ))
-  simp at h
+  simp only [ne_eq, one_ne_zero, not_false_eq_true, div_self, C_1, Fin.isValue, pow_one, one_mul,
+    zero_add] at h
   exact h
 
--- cyclic-2
 example :
   lex.IsGroebnerBasis
   ({X 0 + X 1, X 1 ^ 2 + 1} : Set <| MvPolynomial (Fin 2) ℚ)
   (Ideal.span ({X 0 + X 1, X 0 * X 1 - 1} : Set <| MvPolynomial (Fin 2) ℚ)) := by
   add_gb_hyp h ({X 0 + X 1, X 0 * X 1 - 1} : Set (MvPolynomial (Fin 2) ℚ))
-  simp at h
+  simp only [ne_eq, one_ne_zero, not_false_eq_true, div_self, C_1, Fin.isValue, pow_one, one_mul,
+    zero_add] at h
   exact h
 
--- cyclic-3
-set_option maxHeartbeats 20000000 in
-example :
-    letI inputs := ({X 0 + X 1 + X 2,
-                     X 0 * X 1 + X 1 * X 2 + X 2 * X 0,
-                     X 0 * X 1 * X 2 - 1} : Set <| MvPolynomial (Fin 3) ℚ)
-    ∃ (G : Set <| MvPolynomial (Fin 3) ℚ),
-    lex.IsGroebnerBasis G (Ideal.span inputs) := by
-    add_gb_hyp h ({X 0 + X 1 + X 2,
-                     X 0 * X 1 + X 1 * X 2 + X 2 * X 0,
-                     X 0 * X 1 * X 2 - 1} : Set <| MvPolynomial (Fin 3) ℚ)
-    exact
-      Exists.intro _ h
-
--- cyclic-4
-set_option maxHeartbeats 200000000 in
-example :
-    letI inputs := ({X 0 + X 1 + X 2 + X 3,
-                     X 0*X 1 + X 1*X 2 + X 2*X 3 + X 3*X 0,
-                     X 0*X 1*X 2 + X 1*X 2*X 3 + X 2*X 3*X 0 + X 3*X 0*X 1,
-                     X 0*X 1*X 2*X 3 - 1} : Set <| MvPolynomial (Fin 4) ℚ)
-    ∃ (G : Set <| MvPolynomial (Fin 4) ℚ),
-    lex.IsGroebnerBasis G (Ideal.span inputs) := by
-    set_option synthInstance.maxSize 1024 in add_gb_hyp h ({X 0 + X 1 + X 2 + X 3,
-                   X 0*X 1 + X 1*X 2 + X 2*X 3 + X 3*X 0,
-                   X 0*X 1*X 2 + X 1*X 2*X 3 + X 2*X 3*X 0 + X 3*X 0*X 1,
-                   X 0*X 1*X 2*X 3 - 1} : Set <| MvPolynomial (Fin 4) ℚ)
-    exact
-      Exists.intro _ h
-
--- cyclic-5
--- set_option maxHeartbeats 5000000000 in
--- set_option maxRecDepth 500000000 in
--- lemma cyclic5:
---     letI inputs := ({
---       X 0 + X 1 + X 2 + X 3 + X 4,
---       X 0*X 1 + X 1*X 2 + X 2*X 3 + X 3*X 4 + X 4*X 0,
---       X 0*X 1*X 2 + X 1*X 2*X 3 + X 2*X 3*X 4 + X 3*X 4*X 0 + X 4*X 0*X 1,
---       X 0*X 1*X 2*X 3 + X 1*X 2*X 3*X 4 + X 2*X 3*X 4*X 0 + X 3*X 4*X 0*X 1 + X 4*X 0*X 1*X 2,
---       X 0*X 1*X 2*X 3*X 4 - 1
---     } : Set <| MvPolynomial (Fin 5) ℚ)
---     ∃ (G : Set <| MvPolynomial (Fin 5) ℚ),
---     lex.IsGroebnerBasis G (Ideal.span inputs) := by
---     set_option synthInstance.maxSize 1024 in add_gb_hyp h ({
---       X 0 + X 1 + X 2 + X 3 + X 4,
---       X 0*X 1 + X 1*X 2 + X 2*X 3 + X 3*X 4 + X 4*X 0,
---       X 0*X 1*X 2 + X 1*X 2*X 3 + X 2*X 3*X 4 + X 3*X 4*X 0 + X 4*X 0*X 1,
---       X 0*X 1*X 2*X 3 + X 1*X 2*X 3*X 4 + X 2*X 3*X 4*X 0 + X 3*X 4*X 0*X 1 + X 4*X 0*X 1*X 2,
---       X 0*X 1*X 2*X 3*X 4 - 1
---     } : Set <| MvPolynomial (Fin 5) ℚ)
---     exact
---       Exists.intro _ h
-
--- #print axioms cyclic5
-
-example :
-  letI inputs := ({X 0 - C (2/3 : ℚ), X 1 + C (4/5 : ℚ)}: Set <| MvPolynomial (Fin 2) ℚ)
-    ∃ (G : Set <| MvPolynomial (Fin 2) ℚ),
-    lex.IsGroebnerBasis G (Ideal.span inputs) := by
-    add_gb_hyp h ({X 0 - C (2/3 : ℚ), X 1 + C (4/5 : ℚ)} : Set <| MvPolynomial (Fin 2) ℚ)
-    exact
-      Exists.intro
-        {0 + C (1 / 1) * X 0 ^ 1 + C (-2 / 3), 0 + C (1 / 1) * X 1 ^ 1 + C (4 / 5)} h
-
--- Katsura-1
-example :
-    letI inputs := ({
-      X 0 + 2 * X 1 - 1,
-      X 0^2 - X 0 + 2 * X 1^ 2
-    } : Set <| MvPolynomial (Fin 2) ℚ)
-    ∃ (G : Set <| MvPolynomial (Fin 2) ℚ),
-    lex.IsGroebnerBasis G (Ideal.span inputs) := by
-
-    set_option synthInstance.maxSize 1024 in add_gb_hyp h ({
-      X 0 + 2 * X 1 - 1,
-      X 0^2 - X 0 + 2 * X 1^ 2
-    } : Set <| MvPolynomial (Fin 2) ℚ)
-    exact
-      Exists.intro _ h
-
-
-
--- Katsura-2
-set_option maxHeartbeats 2000000 in
-example :
-    letI inputs := ({
-      X 0 + 2 * X 1 + 2 * X 2 - 1,
-      X 0^2 + 2 * X 1^2 + 2 * X 2^2 - X 0,
-      2 * X 0 * X 1 + 2 * X 1 * X 2 - X 1
-    } : Set <| MvPolynomial (Fin 3) ℚ)
-    ∃ (G : Set <| MvPolynomial (Fin 3) ℚ),
-    lex.IsGroebnerBasis G (Ideal.span inputs) := by
-
-    set_option synthInstance.maxSize 1024 in add_gb_hyp h ({
-      X 0 + 2 * X 1 + 2 * X 2 - 1,
-      X 0^2 + 2 * X 1^2 + 2 * X 2^2 - X 0,
-      2 * X 0 * X 1 + 2 * X 1 * X 2 - X 1
-    } : Set <| MvPolynomial (Fin 3) ℚ)
-    exact
-      Exists.intro _ h
-
--- Katsura-3
-set_option maxHeartbeats 2000000 in
-example :
-    letI inputs := ({
-      X 0 + 2 * X 1 + 2 * X 2 + 2 * X 3 - 1,
-      X 0^2 - X 0 + 2 * X 1^2 + 2 * X 2^2 + 2* X 3^2,
-      2 * X 0 * X 1 + 2 * X 1 * X 2 - X 1 + 2 * X 2 * X 3,
-      2*X 0*X 2 + X 1^2 + 2*X 1*X 3 - X 2
-    } : Set <| MvPolynomial (Fin 3) ℚ)
-    ∃ (G : Set <| MvPolynomial (Fin 3) ℚ),
-    lex.IsGroebnerBasis G (Ideal.span inputs) := by
-
-    set_option synthInstance.maxSize 1024 in add_gb_hyp h ({
-      X 0 + 2 * X 1 + 2 * X 2 + 2 * X 3 - 1,
-      X 0^2 - X 0 + 2 * X 1^2 + 2 * X 2^2 + 2* X 3^2,
-      2 * X 0 * X 1 + 2 * X 1 * X 2 - X 1 + 2 * X 2 * X 3,
-      2*X 0*X 2 + X 1^2 + 2*X 1*X 3 - X 2
-    } : Set <| MvPolynomial (Fin 3) ℚ)
-    exact
-      Exists.intro _ h
-
--- Katsura-4
-set_option maxHeartbeats 5000000 in
-example :
-    letI inputs := ({
-      X 0 + 2 * X 1 + 2 * X 2 + 2 * X 3 + 2 * X 4 - 1,
-      X 0^2 - X 0 + 2 * X 1^2 + 2 * X 2^2 + 2 * X 3^2 + 2 * X 4^2,
-      2 * X 0 * X 1 + 2 * X 1 * X 2 - X 1 + 2 * X 2 * X 3 + 2 * X 3 * X 4,
-      2 * X 0 * X 2 + X 1^2 + 2 * X 1 * X 3 + 2 * X 2 * X 4 - X 2,
-      2 * X 0 * X 3 + 2 * X 1 * X 2 + 2 * X 1 * X 4 - X 3
-    } : Set <| MvPolynomial (Fin 5) ℚ)
-    ∃ (G : Set <| MvPolynomial (Fin 5) ℚ),
-    lex.IsGroebnerBasis G (Ideal.span inputs) := by
-
-    set_option synthInstance.maxSize 1024 in add_gb_hyp h ({
-      X 0 + 2 * X 1 + 2 * X 2 + 2 * X 3 + 2 * X 4 - 1,
-      X 0^2 - X 0 + 2 * X 1^2 + 2 * X 2^2 + 2 * X 3^2 + 2 * X 4^2,
-      2 * X 0 * X 1 + 2 * X 1 * X 2 - X 1 + 2 * X 2 * X 3 + 2 * X 3 * X 4,
-      2 * X 0 * X 2 + X 1^2 + 2 * X 1 * X 3 + 2 * X 2 * X 4 - X 2,
-      2 * X 0 * X 3 + 2 * X 1 * X 2 + 2 * X 1 * X 4 - X 3
-    } : Set <| MvPolynomial (Fin 5) ℚ)
-
-    exact
-      Exists.intro _ h
-
+/-Some examples that verify two ideals are equal-/
 example :
   Ideal.span ({X 0 + X 1^2, X 1 }) =
     Ideal.span ({X 0, X 1 } : Set (MvPolynomial (Fin 3) ℚ)) := by
